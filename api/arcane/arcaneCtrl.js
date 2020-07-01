09b526c9e6bad1fdd9fd96b7e0208b2016969410
ltnscp9028 <ltnscp9028@gmail.com>
2020-07-01 12:57:37 +0000

    Arcane-Weapon

--- api/arcane/arcaneCtrl.js
+++ api/arcane/arcaneCtrl.js
@@ -4,20 +4,36 @@ const test = (req,res) => {
 }
 
 const upStat = (req,res) => {
-    const arr = [[3,180],[4,270],[7,470]];
+    const arr = [[3,180,"100%작"],[4,270,"70%작"],[7,470,"30%작"]];
     const {mainStat,subStat,upgradeCount} = req.params;
     const jack = (mainStat-subStat)/upgradeCount;
-    let result;
+    let result = "섞작";
     for(let i=0;i<3;i++)
         for(let j=0;j<2;j++){
             if(arr[i][j]==jack){
-                result = i==0?"100작":i==1?"70%작":"30%작";
+                result = arr[i][2];
                 break;
             }
         }
     console.log(result);
-    result = result ?  result : "섞작";
     res.send(result);
     res.end();
 }
-module.exports = {test,upStat};
+
+const weaponReinforce = (req,res) => {
+    const arr = [[1,50,"100%작"],[2,100,"70%작"],[3,150,"30%작"],[4,200,"15%작"]];
+    const {mainStat, subStat, upgradeCount} = req.params;
+    const jack = (mainStat-subStat)/upgradeCount;
+    let result = "섞작";
+    for(let i=0;i<4;i++)
+        for(let j=0;j<2;j++){
+            if(arr[i][j]==jack){
+                result = arr[i][2];
+                break;
+            }
+        }
+    res.send(result);
+    res.end();
+}
+
+module.exports = {test,upStat,weaponReinforce};
--- api/arcane/index.js
+++ api/arcane/index.js
@@ -4,4 +4,5 @@ const ctrl = require("./arcaneCtrl");
 
 router.get("/test",ctrl.test);
 router.get("/reinforce/armor/:mainStat/:subStat/:upgradeCount",ctrl.upStat);
+router.get("/reinforce/weapon/:mainStat/:subStat/:upgradeCount",ctrl.weaponReinforce);
 module.exports = router;
